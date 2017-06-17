5b70adf58d1e9fb271b4ade8ad3ab5a305408f85
yongqi <yongqi_lee@yahoo.com>
2017-06-17 13:53:30 +0800

    try training on my own pc

--- convnet-training/compile.sh
+++ convnet-training/compile.sh
@@ -7,7 +7,7 @@ if uname | grep -q Darwin; then
   CUDNN_LIB_DIR=/usr/local/cudnn/v5/lib
 elif uname | grep -q Linux; then
   CUDA_LIB_DIR=/usr/local/cuda/lib64
-  CUDNN_LIB_DIR=/usr/local/cudnn/v5/lib64
+  CUDNN_LIB_DIR=/home/yongqi/Downloads/cuda/lib64
 fi
 
-nvcc -ccbin /usr/bin/g++ -std=c++11 -O3 -o marvin marvin.cu -I./util -I/usr/local/cuda/include -I/usr/local/cudnn/v5/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand -D_MWAITXINTRIN_H_INCLUDED `pkg-config --cflags opencv` `pkg-config --libs opencv`
+nvcc -ccbin /usr/bin/g++ -std=c++11 -O3 -o marvin marvin.cu -I./util -I/usr/local/cuda/include -I/home/yongqi/Downloads/cuda/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand -D_MWAITXINTRIN_H_INCLUDED `pkg-config --cflags opencv` `pkg-config --libs opencv`
